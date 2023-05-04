9c6f362b4d1ee6ada7274bcefeb1f31c6f23ad32
hulk510 <dorian.51069@gmail.com>
2023-05-04 11:33:10 +0900

    Initial Commit

---
+++ kakinsitekure/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  kakinsitekure
+//
+//  Created by 後藤遥 on 2023/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kakinsitekure/kakinsitekureApp.swift
@@ -0,0 +1,17 @@
+//
+//  kakinsitekureApp.swift
+//  kakinsitekure
+//
+//  Created by 後藤遥 on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct kakinsitekureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
